3b4e0bcdd5b04ee4b7f086d8cc7dd5c77fdb8051
Shane Holloway <shane.holloway@ieee.org>
2020-03-28 17:07:44 -0600

    Neat stuff

--- esm/index.mjs
+++ esm/index.mjs
@@ -2,8 +2,23 @@ const delay = (( ms=1000 ) =>(
   new Promise(y =>
     setTimeout(y, ms) ) ) );
 
-async function * as_async_iter(ao_iterable) {
+async function * as_ao_iter(ao_iterable) {
+  if (undefined !== ao_iterable.tail) {
+    ao_iterable = ao_tail_obj(ao_iterable);}
+
   yield * ao_iterable;}
+
+async function * ao_tail_obj(ao_obj) {
+  if (undefined === ao_obj) {ao_obj = this;}
+
+  while (ao_obj && true !== ao_obj.done) {
+    yield ao_obj.value;
+    ao_obj = await ao_obj.tail;} }
+
+function is_ao_iterable(v) {
+  if (undefined !== v[Symbol.asyncIterator] || v.next || v.tail) {
+    return true}
+  return false}
 
 
 const deferred = ((() => {
@@ -48,7 +63,7 @@ function ao_deferred(init_ao) {
 
 function ao_tee(ao_iter, only_next_) {
   only_next_ = !! only_next_;
-  ao_iter = as_async_iter(ao_iter);
+  ao_iter = as_ao_iter(ao_iter);
 
   const aod = ao_deferred (async ( ao ) => {
     for await (const tip of ao_iter) {
@@ -73,65 +88,81 @@ async function * ao_tee_dispatch(aod, only_next) {
       return await aod.done}
     yield aod.tip;} }
 
+const ao_watch_obj0 ={
+  [Symbol.asyncIterator]: ao_tail_obj};
 
-async function _ao_watcher(obj, ao_iter, on_update) {
+async function ao_watch(...args) {
+  let ao_obj={__proto__: ao_watch_obj0}, ao_iter, on_update;
+  for (const ea of args) {
+    if ('function' === typeof ea) {
+      on_update = ea;}
+    else if (is_ao_iterable(ea)) {
+      ao_iter = as_ao_iter(ea);}
+    else ao_obj = ea;}
+
+  return _ao_walker(ao_obj, ao_iter, on_update) }
+
+
+async function _ao_walker(ao_obj, ao_iter, on_update) {
   const {value, done} = await ao_iter.next();
-  obj.value = value;
-  obj.done = done;
-  obj.tail = done ? Promise.resolve(obj)
-    : _ao_watcher(obj, ao_iter, on_update);
+  ao_obj.value = value;
+  ao_obj.done = done;
+  ao_obj.tail = done ? Promise.resolve(ao_obj)
+    : _ao_walker(ao_obj, ao_iter, on_update);
 
   if (undefined !== on_update) {
-    await on_update(value, obj); }
+    ao_obj.value = await on_update(value, ao_obj); }
 
-  return obj}
+  return ao_obj}
 
 async function as_ao_watch(arg) {
   arg = await arg;
-  if (undefined !== arg.tail) {
-    return arg }// already an ao_watch instance
+  if (undefined !== arg && null !== arg) {
+    if (undefined !== arg.tail) {
+      return arg }// already an ao_watch instance
 
-  return await _ao_watcher({}, as_async_iter(arg)) }
+    if (is_ao_iterable(arg)) {
+      return await _ao_walker(
+        {__proto__: ao_watch_obj0}
+      , as_ao_iter(arg)) } }
 
-function _ao_wrap_watcher(_ao_compute_with) {
-  return fn_compute =>
-    _ao_watcher({}, _ao_compute_with(fn_compute)) }
+  return {value: arg, done: true} }
 
-function ao_watch_compute_kw(by_kw) {
-  return _ao_wrap_watcher(
-    ao_compute_entries(Object.entries(by_kw)) ) }
+const _e_value = e => e.value;
 
-function ao_compute_entries(by_entries) {
-  return _ao_compute_with.bind(null,
-    _ao_watch_deps_map(by_entries)
-  , _ao_compute_kw) }
+function ao_track_updates_kw(kw) {
+  kw = Object.entries(kw);
+  return _ao_track_updates_kw(ao_deps_map(kw)) }
 
-async function _ao_watch_deps_map(by_entries) {
+async function * _ao_track_updates_kw(watch_deps) {
+  watch_deps = await watch_deps;
+
+  while (true) {
+     {
+      const deps = {};
+      for (const e of watch_deps.entries()) {
+        deps[e[0]] = e[1].value;}
+
+      yield deps;}
+
+    await _ao_await_any_dep(watch_deps.values()); } }
+
+async function ao_deps_map(by_entries) {
   const watch_deps = new Map();
   for (const [name, each] of by_entries) {
-    watch_deps.set(name, await as_ao_watch(await each)); }
+    watch_deps.set(name, await as_ao_watch(each)); }
   return watch_deps}
 
 
-async function _ao_watch_join(watch_deps) {
+
+async function _ao_await_any_dep(iter_deps) {
   await new Promise (( resolve ) => {
-    for (const d of watch_deps.values()) {
-      d.tail.then(resolve);} }); }
+    for (const {tail} of iter_deps) {
+      if (undefined !== tail) {
+        tail.then(resolve);} } }); }
 
-async function * _ao_compute_with(watch_deps, _ao_compute, fn_compute, only_next) {
-  watch_deps = await watch_deps;
-  if (only_next) {
-    await _ao_watch_join(watch_deps); }
-
-  while (true) {
-    yield _ao_compute(watch_deps, fn_compute);
-    await _ao_watch_join(watch_deps); } }
-
-function _ao_compute_kw(watch_deps, fn_compute) {
-  const deps = {};
-  for (const [k,d] of watch_deps.entries()) {
-    deps[k] = d.value;}
-  return fn_compute(deps)}
+function ao_track_kw(kw) {
+  return ao_tee(ao_track_updates_kw(kw)) }
 
 async function * ao_updates(init_ao) {
   const aod = ao_deferred(init_ao);
@@ -158,59 +189,120 @@ async function * ao_dispatch(aod, xform) {
 
     yield v_cur;} }
 
-function ao_dom_events(...args) {
-  return ao_tee(ao_dom_updates(...args)) }
+const _en_click = ['click'];
+const _en_input = ['input', 'change'];
+const _e_no_default = e => e.preventDefault();
+const _opt_unpack = ({text, value}) =>({text, value});
+const _dom_std_args ={
+  _:[_en_click]
+, 'input':[_en_input, _e_value]
+, 'output':[_en_input, _e_value]
+, 'input,number':[_en_input, e => e.valueAsNumber]
+, 'input,range':[_en_input, e => e.valueAsNumber]
+, 'input,button':[_en_click, _e_value]
+, 'input,submit':[_en_click, _e_value]
+, 'input,checkbox':[_en_input, e => e.checked]
+, 'input,radio':[_en_input, e => e.checked]
+, 'input,date':[_en_input, e => e.valueAsDate]
+, 'input,time':[_en_input, e => e.valueAsNumber]
+, 'input,file':[_en_input, e => e.multiple ? e.files : e.files[0]]
+, 'textarea':[_en_input, _e_value]
+, 'select':{
+      evt_names: _en_input
+    , on_evt(e) {
+        const res = Array.from(e.selectedOptions, _opt_unpack);
+        return e.multiple ? res : res[0]} }
 
-function ao_dom_updates(elem, evt_list='input', fn_compute='value') {
-   {
-    if ('string' === typeof elem) {
-      elem = document.querySelector(elem);}
+, 'form':{
+      evt_names: _en_input
+    , on_evt: e => new FormData(e)
+    , on_add(e) {e.addEventListener('submit', _e_no_default);} } };
 
-    if ('string' === typeof evt_list) {
-      evt_list = evt_list.split(/\s+/);}
 
-    if ('string' === typeof fn_compute) {
-      const attr = fn_compute;
-      fn_compute = elem => elem[attr];} }
+function _dom_builtin(std, elem) {
+  let {tagName: tag, type} = elem;
+  tag = tag.toLowerCase();
 
+  const res =
+    type && std[`${tag},${type.toLowerCase()}`]
+    || std[tag] || std._;
+
+  return Array.isArray(res)
+    ?{elem, evt_names: res[0], on_evt: res[1]}
+    :{elem, ... res} }
+
+
+function _dom_unpack_args(std, elem, args) {
+  if ('string' === typeof elem) {
+    elem = document.querySelector(elem);}
+
+  return args && args.length
+    ?{elem, evt_names: args[0], on_evt: args[1]}
+    : _dom_builtin(std, elem)}
+
+
+
+const _dom_std_unpack_args = 
+  _dom_unpack_args.bind(null, _dom_std_args);
+
+function ao_dom_events(elem, ...args) {
+  return ao_tee(_ao_dom_updates(
+    _dom_std_unpack_args(elem, args)) ) }
+
+
+
+function _ao_dom_updates({elem, evt_names, on_evt, on_calc, on_add, on_remove}) {
+  if (!Array.isArray(evt_names)) {
+    evt_names = (evt_names || 'click').split(/\s+/);}
+
+  const extra = on_evt || {};
+  if ('function' !== typeof on_evt) {
+    on_evt = extra.on_evt;}
 
   return ao_updates ((async function * ( ao ) {
-    function _update() {
-      ao.update(elem); }
+    const _update = on_evt
+      ? evt => ao.update(on_evt(elem, evt))
+      : ()=> ao.update(elem);
 
-    for (const evt of evt_list) {
-      elem.addEventListener(evt, _update); }
+    if (extra.on_add) {
+      extra.on_add(elem);}
+
+    for (const e of evt_names) {
+      elem.addEventListener(e, _update); }
 
     try {
       _update(elem);
-      yield fn_compute;}
+      yield extra.on_calc;}
 
     finally {
-      for (const evt of evt_list) {
-        elem.removeEventListener(evt, _update); } } }).bind(this)) }
+      for (const e of evt_names) {
+        elem.removeEventListener(e, _update); }
 
-(async ()=>{
-  while ('complete' !== document.readyState) {
-    await delay(10);}
+      if (extra.on_remove) {
+        extra.on_remove(elem);} } }).bind(this)) }
+//# sourceMappingURL=index.mjs.map
 
-  console.log("READY FOR SETUP");
+const dom_loaded = new Promise(resolve =>
+  window.addEventListener('DOMContentLoaded', resolve) );
 
-  const ao_fn = ao_watch_compute_kw({
-    red: ao_dom_events('#red', 'input', elem => + elem.value)
-  , green: ao_dom_events('#green', 'input', elem => + elem.value)
-  , blue: ao_dom_events('#blue', 'input', elem => + elem.value)
-  , });//pulse: ao_pulse @ 2000, true
+{(async ()=>{
+  await dom_loaded;
 
-  const ao_color = await ao_fn(recompute_color);
-  while (true) {
-    console.log('tick:', ao_color);
-    await delay(15000);} })();
+  const ao_deps = ao_track_kw({
+    red: ao_dom_events('#red')
+  , green: ao_dom_events('#green')
+  , blue: ao_dom_events('#blue') });
 
+  const ao_color = await ao_watch(ao_deps,
+    (({ red, green, blue }) =>
+      `rgb(${[0|red, 0|green, 0|blue]})`) );
 
-async function recompute_color({red, green, blue, pulse}) {
-  const color = `rgb(${[0|red, 0|green, 0|blue]})`;
-  console.log("RE", color);
-  const e_div = document.querySelector('#output');
-  e_div.style.backgroundColor = color;
-  return color}
+  const e_div = document.querySelector('#target');
+  ao_watch(ao_color, color =>
+    e_div.style.backgroundColor = color);
+
+  {(async ()=>{
+    while (true) {
+      console.log('tick:', ao_color.value);
+      await delay(1500);} })();} })();}
 //# sourceMappingURL=index.mjs.map
